617b9c3c5d8ac70faea1168dfd287a80184868e2
Martin Prévost <martin.mprevost@gmail.com>
2024-02-28 16:03:05 +0100

    Transform in class ES6

--- front-end/src/app/scripts/score.js
+++ front-end/src/app/scripts/score.js
@@ -1,21 +1,21 @@
 import { parseUrl } from "./utils";
-import template from "../views/game.html"
-// TODO #class: use the ES6 class keyword
-/* class ScoreComponent constructor */
-export function ScoreComponent() {
-  // TODO #extends: call super(template)
-  var params = parseUrl();
-  
-  this.template = template;
-  this.name = params.name;
-  this.size = parseInt(params.size);
-  this.time = parseInt(params.time);
-}
+import template from "../views/score.html"
+import { Component } from "./component";
 
-// TODO #class: turn function into a method of ScoreComponent
-/* method ScoreComponent.init */
-ScoreComponent.prototype.init = function init() {
-  document.getElementById("name").innerText = this.name;
-  document.getElementById("size").innerText = this.size;
-  document.getElementById("time").innerText = this.time;
-};
+/* class ScoreComponent constructor */
+export class ScoreComponent extends Component {
+  constructor() {
+    super(template);
+    var params = parseUrl();
+    
+    this.name = params.name;
+    this.size = parseInt(params.size);
+    this.time = parseInt(params.time);
+  }
+  
+  init() {
+    document.getElementById("name").innerText = this.name;
+    document.getElementById("size").innerText = this.size;
+    document.getElementById("time").innerText = this.time;
+  }
+}
