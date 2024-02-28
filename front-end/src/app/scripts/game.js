778f6d2fd65f46cfbcea943d6346940abbd03107
Martin Prévost <martin.mprevost@gmail.com>
2024-02-28 16:12:07 +0100

    change var to let or const

--- front-end/src/app/scripts/game.js
+++ front-end/src/app/scripts/game.js
@@ -14,7 +14,7 @@ import card7 from "/src/assets/cards/card-7.png";
 import card8 from "/src/assets/cards/card-8.png";
 import card9 from "/src/assets/cards/card-9.png";
 
-var CARD_TEMPLATE = ""
+const CARD_TEMPLATE = ""
   .concat('<main class="card-cmp">')
   .concat('  <div class="card-wrapper">')
   .concat('    <img class="card front-face" alt="card" />')
@@ -22,7 +22,7 @@ var CARD_TEMPLATE = ""
   .concat("  </div>")
   .concat("</main>");
 
-var environment = {
+const environment = {
   api: {
     host: "http://localhost:8081",
   },
@@ -33,12 +33,12 @@ export class GameComponent extends Component {
   constructor() {
     super(template);
     // gather parameters from URL
-    var params = parseUrl();
-    
+    const params = parseUrl();
+
     this._name = params.name;
     this._size = parseInt(params.size) || 9;
     this._flippedCard = null;
-    this._matchedPairs = 0;  
+    this._matchedPairs = 0;
   }
 
   init() {
@@ -48,47 +48,40 @@ export class GameComponent extends Component {
       function (config) {
         this._config = config;
         this._boardElement = document.querySelector(".cards");
-  
+
         // create cards out of the config
         this._cards = [];
         // TODO #functional-programming: use Array.map() instead.
-        for (var i in this._config.ids) {
+        for (let i in this._config.ids) {
           this._cards[i] = new CardComponent(this._config.ids[i]);
         }
-  
-        // TODO #functional-programming: use Array.forEach() instead.
-        // TODO #let-const: replace var with let.
-        for (var i in this._cards) {
-          var card = this._cards[i];
-  
-          // TODO #let-const: extract function _appendCard (ie: copy its body here and remove the function)
-          this._appendCard(card);
-        }
-  
-        this.start();
-      }.bind(this)
-    );
-  }
 
-  _appendCard(card) {
-    this._boardElement.appendChild(card.getElement());
-  
-    card.getElement().addEventListener(
-      "click",
-      // TODO #arrow-function: use arrow function instead.
-      function () {
-        this._flipCard(card);
+        // TODO #functional-programming: use Array.forEach() instead.
+        for (let i in this._cards) {
+          const card = this._cards[i];
+          this._boardElement.appendChild(card.getElement());
+
+          card.getElement().addEventListener(
+            "click",
+            // TODO #arrow-function: use arrow function instead.
+            function () {
+              this._flipCard(card);
+            }.bind(this)
+          );
+        }
+
+        this.start();
       }.bind(this)
     );
   }
 
   start() {
     this._startTime = Date.now();
-    var seconds = 0;
+    let seconds = 0;
     // TODO #template-literals:  use template literals (backquotes)
     document.querySelector("nav .navbar-title").textContent =
       "Player: " + this._name + ". Elapsed time: " + seconds++;
-  
+
     this._timer = setInterval(
       // TODO #arrow-function: use arrow function instead.
       function () {
@@ -101,18 +94,18 @@ export class GameComponent extends Component {
   }
 
   fetchConfig(cb) {
-    var xhr =
+    const xhr =
       typeof XMLHttpRequest != "undefined"
         ? new XMLHttpRequest()
         : new ActiveXObject("Microsoft.XMLHTTP");
-  
+
     // TODO #template-literals:  use template literals (backquotes)
     xhr.open("get", environment.api.host + "/board?size=" + this._size, true);
-  
+
     // TODO #arrow-function: use arrow function instead.
     xhr.onreadystatechange = function () {
-      var status;
-      var data;
+      let status;
+      let data;
       // https://xhr.spec.whatwg.org/#dom-xmlhttprequest-readystate
       if (xhr.readyState == 4) {
         // `DONE`
@@ -129,13 +122,15 @@ export class GameComponent extends Component {
   }
 
   goToScore() {
-    var timeElapsedInSeconds = Math.floor((Date.now() - this._startTime) / 1000);
+    const timeElapsedInSeconds = Math.floor(
+      (Date.now() - this._startTime) / 1000
+    );
     clearInterval(this._timer);
-  
+
     setTimeout(
       // TODO #arrow-function: use arrow function instead.
       function () {
-        var scorePage = "./#score";
+        const scorePage = "./#score";
         // TODO #template-literals:  use template literals (backquotes)
         window.location =
           scorePage +
@@ -154,36 +149,36 @@ export class GameComponent extends Component {
     if (this._busy) {
       return;
     }
-  
+
     if (card.flipped) {
       return;
     }
-  
+
     // flip the card
     card.flip();
-  
+
     // if flipped first card of the pair
     if (!this._flippedCard) {
       // keep this card flipped and wait for the second card of the pair
       this._flippedCard = card;
     } else {
       // second card of the pair flipped...
-  
+
       // if cards are the same
       if (card.equals(this._flippedCard)) {
         this._flippedCard.matched = true;
         card.matched = true;
         this._matchedPairs += 1;
-  
+
         // reset flipped card for the next turn.
         this._flippedCard = null;
-  
+
         if (this._matchedPairs === this._size) {
           this.goToScore();
         }
       } else {
         this._busy = true;
-  
+
         // cards did not match
         // wait a short amount of time before hiding both cards
         setTimeout(
@@ -193,7 +188,7 @@ export class GameComponent extends Component {
             this._flippedCard.flip();
             card.flip();
             this._busy = false;
-  
+
             // reset flipped card for the next turn.
             this._flippedCard = null;
           }.bind(this),
@@ -205,7 +200,7 @@ export class GameComponent extends Component {
 }
 
 // TODO #card-component: Change images location to /app/components/game/card/assets/***.png
-var CARDS_IMAGE = [
+const CARDS_IMAGE = [
   back,
   card0,
   card1,
